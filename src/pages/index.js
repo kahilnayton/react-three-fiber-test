eb493363ab34bd26e97dcb4567962f99c1d46f14
kahil nayton <kahilnayton@gmail.com>
2020-07-29 11:43:19 -0400

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,49 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useRef } from "react"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { Canvas, extend, useRender, useThree } from "react-three-fiber"
+import { useSpring, a } from "react-spring/three"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const Box = () => {
+  const [hovered, setHovered] = useState(false)
+  const [active, setActive] = useState(false)
+  const props = useSpring({
+    scale: active ? [1, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? "grey" : "hotpink",
+  })
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  extend({ OrbitControls })
 
-export default IndexPage
+  const Controls = () => {
+    const orbitRef = useRef()
+    const { camera, gl } = useThree()
+
+    useRender(() => {
+      orbitRef.current.update()
+    })
+
+    return <orbitControls args={[camera, gl.domElement]} ref={orbitRef} />
+  }
+
+  return (
+    <a.mesh
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <a.meshBasicMaterial attach="material" color={props.color} />
+    </a.mesh>
+  )
+}
+
+export default () => {
+  return (
+    <>
+      <Canvas>
+        {/* <Controls /> */}
+        <Box />
+      </Canvas>
+    </>
+  )
+}
